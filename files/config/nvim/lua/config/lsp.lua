8a6344160d7a9866da65379eb9c63c458e0cb88c
afpacket <andrewfox312@gmail.com>
2025-10-20 12:24:38 -0500

    add telescope, add a few initial mappings for lsp navigation

--- files/config/nvim/lua/config/lsp.lua
+++ files/config/nvim/lua/config/lsp.lua
@@ -2,3 +2,12 @@ vim.lsp.enable({
   "gopls",
   "pyright",
 })
+
+vim.g.mapleader = " "
+
+vim.keymap.set("n", "<Leader>d", vim.diagnostic.open_float)
+vim.keymap.set("n", "[d", vim.diagnostic.goto_prev)
+vim.keymap.set("n", "]d", vim.diagnostic.goto_next)
+vim.diagnostic.config({
+   float = { border = "rounded" },
+})
