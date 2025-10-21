977d083e29a5a672f43ed1f29579dd0d0196775e
afpacket <andrewfox312@gmail.com>
2025-10-20 23:16:14 -0500

    update terraform-ls, include nvim-lspconfig plugin

--- files/config/nvim/lsp/terraformls.lua
+++ files/config/nvim/lsp/terraformls.lua
@@ -1,6 +1,7 @@
 return {
   cmd = { "terraform-ls", "serve" },
-  filetypes = { "terraform" },
+  filetypes = { "terraform", "terraform-vars" },
+  root_markers = { ".terraform", ".git" }
   init_options = {
     ignoreSingleFileWarning = true,
   },
