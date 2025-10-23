1e83f2655f5e71ed0d3362dd0059fd312baea8fa
afpacket <andrewfox312@gmail.com>
2025-10-23 16:04:55 -0500

    remove vim-terraform plugin

--- files/config/nvim/lua/plugins/vim-terraform.lua
+++
@@ -1,9 +0,0 @@
-return {
-  -- https://github.com/hashivim/vim-terraform
-  'hashivim/vim-terraform',
-  event = 'VeryLazy',
-  config = function ()
-    -- terraform fmt on save
-    vim.g.terraform_fmt_on_save = 1
-  end
-}
