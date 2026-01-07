40acd77c56686988a131bbe6fcef4a575807cc90
afpacket <andrewfox312@gmail.com>
2026-01-07 12:02:15 -0600

    update to fix lsp config overrides

--- files/config/nvim/lsp/terraformls.lua
+++
@@ -1,8 +0,0 @@
-return {
-  cmd = { "terraform-ls", "serve" },
-  filetypes = { "terraform", "terraform-vars" },
-  root_markers = { ".terraform", ".git" },
-  init_options = {
-    ignoreSingleFileWarning = true,
-  },
-}
